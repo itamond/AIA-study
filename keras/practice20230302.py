bfd5bcf9ff8e62cd1cc9a4c526a53cf8c2735bc5
itamond <baldozaa@gmail.com>
2023-03-02 08:53:38 +0900

    AIA

---
+++ keras/practice20230302.py
@@ -0,0 +1,40 @@
+# 1. 데이터
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+x = np.array([
+    [1,2],
+    [3,4]
+    ])
+
+y = np.array([8,15])
+
+# 2. 모델 구성
+
+model = Sequential()
+model.add(Dense(3, input_dim=2))
+model.add(Dense(5))
+model.add(Dense(3))
+model.add(Dense(1))
+
+# 3. 컴파일, 훈련
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(x, y, epochs=100, batch_size=1)
+
+# 4. 평가, 예측
+
+loss = model.evaluate(x, y)
+print("loss :", loss)
+
+result = model.predict([[5,6]])
+print("[[5,6]]의 예측값 :",  result)
+
+
+
+print(x.shape)
+print(y.shape)

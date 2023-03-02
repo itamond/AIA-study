3ab9157cefbdaca54c978d5dfd86a92cdb6dfdbd
itamond <baldozaa@gmail.com>
2023-03-02 20:06:31 +0900

    AIA

--- keras/practice05.py
+++ keras/practice05.py
@@ -1,52 +1,58 @@
-# 넘파이 리스트 슬라이싱을 이용하여,  100행 1열의 데이터를 랜덤하게 테스트 해보자
+# 넘파이 리스트 슬라이싱을 이용하여,  3행 10열의 데이터를 트랜스포스 한 후 랜덤하게 추출하여 테스트 해보자
 
 
-import sklearn as sl
 import numpy as np
+import sklearn
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
+
 #1. 데이터
 
-x=np.array([range(100), range(200,300)])
 
-x=x.T
+x=np.array([[10,11,12,13,14,15,16,17,18,19],[20,19,18,17,16,15,14,13,12,11],[0,1,2,3,4,5,6,7,8,9]])  # 10행 3열 (10,3)
 
-y=np.array([range(300,400), range(500,600)])
-
-y=y.T
-
-
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= 0.3, shuffle=True, random_state=805)
-
-print("x_train value:",x_train.shape)
-print("x_test value:",x_test.shape)
-print("y_train value:",y_train.shape)
-print("y_test value:",y_test.shape)
+x=x.T  # 3행 10열 (3, 10)
+y=np.array([100,101,102,103,104,105,106,107,108,109])   # 10콤마 벡터 (10, )
 
 
 
+print(y)
+
+
+# print(x.shape)
+# print(y.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, shuffle=True, random_state=111)
+
+# print("x_train:", x_train)
+# print("x_test:", x_test)
+# print("y_train:", y_train)
+# print("y_test:", y_test)
 
 
 #2. 모델 구성
 
 model=Sequential()
-model.add(Dense(3,input_dim=2))
-model.add(Dense(5))
-model.add(Dense(7))
-model.add(Dense(5))
+model.add(Dense(4,input_dim=3))
+model.add(Dense(6))
+model.add(Dense(8))
+model.add(Dense(10))
+model.add(Dense(12))
+model.add(Dense(6))
 model.add(Dense(3))
-model.add(Dense(2))
+model.add(Dense(1))
+
 
 #3. 컴파일, 훈련
 
-model.compile(loss='mse',optimizer='adam')
-model.fit(x_train,y_train, epochs=50, batch_size=1)
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, epochs=100, batch_size=1)
 
 #4. 평가, 예측
 
-loss= model.evaluate(x_test,y_test)
+loss= model.evaluate(x_test, y_test)
 print("loss :", loss)
 
-result= model.predict([[146, 346]])
-print("[[146, 346]]의 예측값 :", result)
+result= model.predict([[20,10,10]])
+print("[[20,10,10]]의 예측값 :", result)
--- keras/test1.py
+++ keras/test1.py
@@ -1,13 +1,13 @@
+
+import sklearn as sl
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-
+from sklearn.model_selection import train_test_split
 #1. 데이터
-x = np.array([range(10), range(21, 31), range(201, 211)])
 
-y = np.array([[1,2,3,4,5,6,7,8,9,10]]) # (1, 10)
+x=np.array(range(100))
 
+print(x)
 
-print (x)
-
-print (y)
+print(x.shape)

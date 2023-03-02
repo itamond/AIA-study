58195a2ff3f84543d81f463c8afd1680c5f482f1
itamond <baldozaa@gmail.com>
2023-03-02 21:41:15 +0900

    AIA

--- keras/나도코딩.py
+++ keras/나도코딩.py
@@ -30,14 +30,22 @@ to_x = 0
 to_y = 0
 
 # 이동 속도
-character_speed = 10
+character_speed = 0.6
+
+#적 enemy 캐릭터
+enemy = pygame.image.load("C:\\study\\enemy.png")
+enemy_size = enemy.get_rect().size # 이미지의 크기를 구해옴
+enemy_width = enemy_size[0] # 캐릭터의 가로 크기
+enemy_height = enemy_size[1] # 캐릭터의 세로 크기
+enemy_x_pos = (screen_width / 2) - (enemy_width / 2) # 화면 가로의 절반 크기에 해당하는 곳에 위치
+enemy_y_pos = (screen_height / 2 )- (enemy_height / 2) # 화면 세로 크기 가장 아래에 해당하는 곳에 위치
+
 
 # 이벤트 루프
 running = True # 게임이 진행중인가?
 while running:
     dt = clock.tick(60) #게임 화면의 초당 프레임 수 설정
         
-    print("fps : " + str(clock.get_fps()))
     
     for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가?
         if event.type == pygame.QUIT : # 창이 닫히는 이벤트가 발생하였는가?
@@ -69,18 +77,32 @@ while running:
     elif character_x_pos > screen_width - character_width:
         character_x_pos = screen_width - character_width
     
-    # 가로 경계값 처리
+    # 세로 경계값 처리
     if character_y_pos < 0:
         character_y_pos = 0
     elif character_y_pos > screen_height - character_height:
         character_y_pos = screen_height - character_height
     
+
+    # 충돌 처리를 위한 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+    
+    enemy_rect = enemy.get_rect()
+    # enemy_rect.left = enemy_x_pos
+    # enemy_rect.top = enemy_y_pos
+    
+    # 충돌 체크
+    if character_rect.colliderect(enemy_rect) :
+        print("충돌했어요")
+        running = False
+    
     
     # screen.fill((0,0,255)) 
     screen.blit(background, (0, 0))  # 배경 그리기
-    
     screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
-    
+    screen.blit(enemy,(enemy_x_pos, enemy_y_pos)) # 적 그리기
     pygame.display.update() #게임화면을 다시 그리기
             
 # pygame 종료
---
+++ keras/혼공파.py
@@ -0,0 +1,29 @@
+print("Hello Python Programming...!")
+print("# 여러 개를 출력합니다.")
+
+
+print("이름\t나이\t지역")
+print("윤인성\t25\t강서구")
+
+
+
+print("""동해물과 백두산이 마르고 닳도록
+      하느님이 보우하사 우리나라 만세
+      무궁화 삼천리 화려강산 대한사람""")
+
+
+print("안녕" + "하세요")
+
+
+print("# 연습 문제")
+print("\\\\\\\\")
+print("-"*8)
+
+print("안녕하세요"[1:3])
+print("안녕하세요"[2:4])
+print("안녕하세요"[1:])
+print("안녕하세요"[:3])
+
+
+print(type(52))
+print(type(52.273))

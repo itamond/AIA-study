e87a4f3d423de6e98188b89b1ae322cc313693f0
itamond <baldozaa@gmail.com>
2023-03-15 11:26:44 +0900

    AIA

--- keras/keras21~30/keras28_dropout12_dacon_wine.py
+++ keras/keras21~30/keras28_dropout12_dacon_wine.py
@@ -8,7 +8,7 @@ from tensorflow.python.keras.models import Sequential, Model, load_model
 from tensorflow.python.keras.layers import Dense, Dropout, Input
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import minmax_scale, maxabs_scale, RobustScaler, StandardScaler
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, RobustScaler, StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from keras.utils import to_categorical
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
@@ -32,10 +32,23 @@ enc = LabelEncoder()
 enc.fit(train_csv['type'])
 train_csv['type'] = enc.transform(train_csv['type'])
 test_csv['type'] = enc.transform(test_csv['type'])
+# DF = DF[DF.메뉴 != '돈까스']
+# [출처] [파이썬] 7. 판다스 - 특정 조건 만족하는 행 삭제하기|작성자 러닝머신
+# idx = train_csv[train_csv['quality']==['3','9']]
+# idx2 = test_csv[test_csv['quality']==['3','9']]
+
+# train_csv = train_csv[train_csv.quality != '3']
+# # train_csv = train_csv[train_csv.quality != '9']
+# drop33 = train_csv[train_csv['quality'].str.contains('3')].index
+# train_csv =train_csv.drop(drop33, inplace=True)
+
+
+print(train_csv.shape)
 
 
 x= train_csv.drop(['quality'], axis=1)
 y= train_csv['quality']
+print(np.unique(y,return_counts=True))
 # test_csv = test_csv.drop(['type'], axis=1)
 
 
@@ -54,17 +67,20 @@ print(np.unique(y,return_counts=True))
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y,
-    random_state=394,
+    random_state=42,
     stratify=y,
     shuffle=True,
     train_size=0.8
 )
 
-scaler = RobustScaler()
+
+scaler = StandardScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 test_csv = scaler.transform(test_csv)
+
+
 
 date = datetime.datetime.now()
 date = date.strftime("%m%d_%H%M")
@@ -72,18 +88,21 @@ filepath = './_save/MCP/keras28/12/'
 filename = '{epoch:04d}-{val_acc:.2f}.hdf5'
 
 
+# model = load_model('./_save/MCP/keras28/12/k28_12_0315_1030_0722-0.61.hdf5')
+
+
 #2. 모델구성
 
 
 input1 = Input(shape=(12,))
-dense1 = Dense(1000,activation='relu')(input1)
+dense1 = Dense(200,activation='relu')(input1)
 drop1 = Dropout(0.5)(dense1)
-dense2 = Dense(700,activation='relu')(drop1)
+dense2 = Dense(100,activation='relu')(drop1)
 drop2 = Dropout(0.5)(dense2)
-dense3 = Dense(500,activation='relu')(drop2)
+dense3 = Dense(200,activation='relu')(drop2)
 drop3 = Dropout(0.5)(dense3)
 dense4 = Dense(100,activation='relu')(drop3)
-drop4 = Dropout(0.5)(dense4)
+drop4 = Dropout(0.2)(dense4)
 dense5 = Dense(40,activation='relu')(drop4)
 dense6 = Dense(20,activation='relu')(dense5)
 output1 = Dense(7,activation='softmax')(dense6)
@@ -110,11 +129,12 @@ mcp = ModelCheckpoint(monitor='val_acc',
 
 hist = model.fit(x_train,y_train,
                  epochs= 5000,
-                 batch_size = 65,
-                 validation_split=0.2,
+                 batch_size = 96,
+                 validation_split=0.3,
                  verbose=1,
                  callbacks=[es,mcp])
 
+# model.save('./_save/keras26_3_save_model.h5')   
 
 #4. 평가, 예측
 
@@ -135,6 +155,10 @@ submission['quality'] = y_submit
 print(y_submit)
 submission.to_csv(path_save+'submission_wine_DO_5.csv')
 
+model.fit(x_train, y_train)
+print(model.feature_importances_)
+
 # print(y_submit)
+
 
 
---
+++ keras/keras21~30/keras29_LabelEncoder.py
@@ -0,0 +1,90 @@
+# 데이콘 따릉이 문제풀이
+import numpy as np
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error 
+import pandas as pd 
+
+
+#1. 데이터
+path='./_data/dacon_wine/'      # .=현 폴더, study    /= 하위폴더
+
+train_csv = pd.read_csv(path + 'train.csv',
+                        index_col=0)                 
+                                                     
+test_csv = pd.read_csv(path + 'test.csv',
+                        index_col=0)                 
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+le.fit(train_csv['type'])   
+
+aaa = le.transform(train_csv['type'])
+print(aaa)
+print(type(aaa))  #<class 'numpy.ndarray'>
+# print(np.unique(aaa, return_counts=True))
+train_csv['type'] = aaa
+test_csv['type'] = le.transform(test_csv['type'])
+#정의->핏->트랜스폼
+
+print(le.transform(['red','white']))    # 변환 전에 뭐였는지 확인하는 법
+
+
+# print(train_csv.columns)
+# print(train_csv.info())    
+# print(type(train_csv))   
+
+
+# x = train_csv.drop(['count'], axis=1)     
+# print(x)
+
+# y = train_csv['count']
+# print(y)
+
+
+# x_train, x_test, y_train, y_test = train_test_split(
+#     x, y,
+#     shuffle=True,
+#     train_size=0.7,
+#     random_state=221
+# )
+
+
+
+
+
+# #2. 모델구성
+# model = Sequential()
+# model.add(Dense(18, input_dim=9))
+# model.add(Dense(27))
+# model.add(Dense(36))
+# model.add(Dense(45))
+# model.add(Dense(36))
+# model.add(Dense(27))
+# model.add(Dense(18))
+# model.add(Dense(9))
+# model.add(Dense(1))
+
+# #3. 컴파일, 훈련
+# model.compile(loss='mse', optimizer='adam')
+# model.fit(x_train, y_train,
+#           epochs= 10,
+#           batch_size=5,
+#           verbose=1)
+
+# #4. 평가, 예측
+
+# loss= model.evaluate(x_test, y_test)
+# print('loss :', loss)
+
+# # y_predict= model.predict(x_test)
+# # loss가 nan으로 뜨는 이유 = 결측치가 너무 많다.
+
+# #loss : 3015.99658203125   랜덤 777  에포 100 배치 사이즈 5
+
+# #loss : 2920.375732421875 랜덤221   에포 100 배치 5
+
+
+# print(y_test)

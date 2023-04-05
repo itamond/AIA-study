ecf03cf960c0c36745e726ce77b0ed89042e366b
ykyk32 <baldozaa@gmail.com>
2023-04-05 09:46:47 +0900

    AIA

--- keras/keras51~/keras59_1_Tokenizer.py
+++ keras/keras51~/keras59_1_Tokenizer.py
@@ -35,7 +35,7 @@ print(x.shape)
 # 이럴경우 첫 0을 삭제하고 리쉐이프 해주면 됨
 
 ####### 2. get_dummies ########
-import pandas as pd
+import pandas as pd          #1차원만 받아서 작업을 한번 더 해야함
 import numpy as np
 
 # x= np.array(x)
@@ -45,7 +45,7 @@ import numpy as np
 # x= np.array(x)
 # print(x)
 # x = pd.get_dummies(np.array(x).reshape(11,))
-# x = pd.get_dummies(np.array(x).ravel()) #flatten과 동일. 넘파이의 flatten
+# x = pd.get_dummies(np.array(x).ravel())        #flatten과 동일. 넘파이의 flatten
 # x = np.array(pd.get_dummies(x[0]))
 # print(x)
 

390f556a47b0697644fa2bfd206326510ad828ef
hwanhee <baldozaa@gmail.com>
2023-05-11 09:02:41 +0000

    AIA

--- tf114/tf04_eager1.py
+++ tf114/tf04_eager1.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 print(tf.__version__)
 
 # 즉시실행모드
-print(tf.executing_eagerly()) #텐서1 = False   텐서2 = True
+print(tf.executing_eagerly()) #텐서1 = False(그래프 연산 방식)   텐서2 = True
 # 즉시실행모드 비활성
 tf.compat.v1.disable_eager_execution() # 텐서 2.0을 텐서1.0 방식으로 사용
 # 즉시실행모드 활성

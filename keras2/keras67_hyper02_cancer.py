38cbe5ff2f3da54723f26d9d0a63ac5efa4f4246
hwanhee <baldozaa@gmail.com>
2023-05-22 19:28:02 +0900

    AIA

--- keras2/keras67_hyper02_cancer.py
+++ keras2/keras67_hyper02_cancer.py
@@ -10,7 +10,7 @@ from tensorflow.keras.optimizers import Adam, RMSprop, Adadelta
 x, y = load_breast_cancer(return_X_y=True)
 x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.8)
 print(x_train.shape)
-
+print(np.unique(y_train, return_counts=True))
 
 # 2. 모델
 def build_model(drop=0.2, optimizer='adam', activation='relu', node1=64, node2=64, node3=64, node4=64, lr = 0.001):
@@ -22,7 +22,7 @@ def build_model(drop=0.2, optimizer='adam', activation='relu', node1=64, node2=6
     x = Dense(node3, activation=activation, name='hidden3')(x)
     x = Dropout(drop)(x)
     x = Dense(node4, activation=activation, name='hidden4')(x)
-    outputs = Dense(1, activation='sigmoid', name='outputs')(x)
+    outputs = Dense(1, activation='softmax', name='outputs')(x)
 
     model = Model(inputs = inputs, outputs = outputs)
     
@@ -32,7 +32,7 @@ def build_model(drop=0.2, optimizer='adam', activation='relu', node1=64, node2=6
 
 
 def create_hyperparameter():
-    batchs = [100, 200, 300, 400, 500]
+    batchs = [10, 20, 30, 40, 50]
     lr = [0.001, 0.005, 0.01]
     optimizers = [Adam(learning_rate=lr), RMSprop(learning_rate=lr), Adadelta(learning_rate=lr)]
     dropouts = [0.2, 0.3, 0.4, 0.5]
@@ -68,11 +68,11 @@ print('acc : ', accuracy_score(y_test, y_predict))
 
 
 
-# 걸린시간 :  4.371129989624023
-# model.best_params_ :  {'optimizer': <keras.optimizer_v2.adadelta.Adadelta object at 0x000001C8ED7D0F10>, 'drop': 0.5, 'batch_size': 200, 'activation': 'relu'}
-# model.best_estimator :  <keras.wrappers.scikit_learn.KerasClassifier object at 0x000001C895C19FD0>
-# model.best_score_ :  0.3692307770252228
-# WARNING:tensorflow:6 out of the last 6 calls to <function Model.make_test_function.<locals>.test_function at 0x000001C90E1D03A0> triggered tf.function retracing. Tracing is expensive and the excessive number of tracings could be due to (1) creating @tf.function repeatedly in a loop, (2) passing tensors with different shapes, (3) passing Python objects instead of tensors. For (1), please define your @tf.function outside of the loop. For (2), @tf.function has experimental_relax_shapes=True option that relaxes argument shapes that can avoid unnecessary retracing. For (3), please refer to https://www.tensorflow.org/guide/function#controlling_retracing and https://www.tensorflow.org/api_docs/python/tf/function for  more details.
-# 1/1 [==============================] - 0s 50ms/step - loss: nan - accuracy: 0.3860
-# model.score :  0.38596490025520325
-# acc :  0.38596491228070173
+# 걸린시간 :  8.619102716445923
+# model.best_params_ : {'optimizer': 'Adam', 'lr': 0.01, 'drop': 0.2, 'batch_size': 200, 'activation': 'elu'}
+# model.best_estimator :  <keras.wrappers.scikit_learn.KerasClassifier object at 0x000001B389E3DD60>
+# model.best_score_ :  0.38461539149284363
+# WARNING:tensorflow:6 out of the last 6 calls to <function Model.make_test_function.<locals>.test_function at 0x000001B39A693E50> triggered tf.function retracing. Tracing is expensive and the excessive number of tracings could be due to (1) creating @tf.function repeatedly in a loop, (2) passing tensors with different shapes, (3) passing Python objects instead of tensors. For (1), please define your @tf.function outside of the loop. For (2), @tf.function has experimental_relax_shapes=True option that relaxes argument shapes that can avoid unnecessary retracing. For (3), please refer to https://www.tensorflow.org/guide/function#controlling_retracing and https://www.tensorflow.org/api_docs/python/tf/function for  more details.
+# 1/1 [==============================] - 0s 50ms/step - loss: nan - accuracy: 0.3246
+# model.score :  0.3245614171028137
+# acc :  0.32456140350877194
